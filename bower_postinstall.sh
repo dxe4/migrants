754c9e895ca24ac7ba995d264e1924cae04490fd
Charalampos Papaloizou <papaloizouc@gmail.com>
2014-12-25 21:12:22 +0000

    added angular resource

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -1,5 +1,7 @@
 # TODO this is crap
 mkdir -p static/lib
-mv bower_components/angularjs/angular.min.js static/lib/
-mv bower_components/angularjs/angular.min.js static/lib/
+cp bower_components/angularjs/angular.min.js static/lib/
+cp bower_components/angularjs/angular.min.js.map static/lib/
+cp bower_components/angular-resource/angular-resource.min.js static/lib/
+cp bower_components/angular-resource/angular-resource.min.js.map static/lib
 
