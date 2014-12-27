66b212f5ef6177f005082a971db696f0a3eae1a2
Charalampos Papaloizou <papaloizouc@gmail.com>
2014-12-27 07:46:50 +0000

    datamaps lib

--- bower_postinstall.sh
+++ bower_postinstall.sh
@@ -4,4 +4,7 @@ cp bower_components/angularjs/angular.min.js static/lib/
 cp bower_components/angularjs/angular.min.js.map static/lib/
 cp bower_components/angular-resource/angular-resource.min.js static/lib/
 cp bower_components/angular-resource/angular-resource.min.js.map static/lib
+cp bower_components/datamaps/dist/datamaps.world.min.js static/lib
+cp bower_components/topojson/topojson.js static/lib
+cp bower_components/d3/d3.js static/lib
 
